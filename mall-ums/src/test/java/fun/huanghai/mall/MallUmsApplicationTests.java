ac04300b091373bda5e1711fac2587a0cd6f95c5
huangh <869144014@qq.com>
2019-12-23 20:08:21 +0800

    配置多环境 公司环境：comp 自己环境：home

--- mall-ums/src/test/java/fun/huanghai/mall/MallUmsApplicationTests.java
+++ mall-ums/src/test/java/fun/huanghai/mall/MallUmsApplicationTests.java
@@ -1,24 +1,35 @@
 package fun.huanghai.mall;
 
 import fun.huanghai.mall.dao.UmsAdminLoginLogMapper;
+import fun.huanghai.mall.dao.UmsRoleDaoExpand;
 import fun.huanghai.mall.ums.pojo.UmsAdminLoginLogExample;
+import fun.huanghai.mall.ums.pojo.UmsRole;
+import fun.huanghai.mall.ums.pojo.UmsRoleExample;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MallUmsApplicationTests {
 
 	@Autowired
-	UmsAdminLoginLogMapper umsAdminLoginLogMapper;
+	@Qualifier("umsRoleDaoExpand")
+	UmsRoleDaoExpand umsRoleDaoExpand;
 
 	@Test
 	public void contextLoads() {
-		UmsAdminLoginLogExample example = new UmsAdminLoginLogExample();
-		System.out.println(umsAdminLoginLogMapper.selectByExample(example));
+		UmsRoleExample example = new UmsRoleExample();
+		List<UmsRole> umsRoles = umsRoleDaoExpand.selectByExample(example);
+		umsRoles.forEach(u -> {
+			System.out.println(u.getName());
+		});
+		System.out.println();
 	}
 
 }
